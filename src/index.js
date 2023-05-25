29324ba9103b23fb45a910933942f81090895fb2
Olga Plotkina <101660609+olga-plotkina@users.noreply.github.com>
2023-05-25 15:54:04 +0100

    change page's maxHeight

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     minWidth: 384,
     maxWidth: 1000,
     minHeight: 448,
-    maxHeight: 1350,
+    maxHeight: 900,
 
     maxShadowOpacity: 0.5, // Half shadow intensity
     showCover: true,
